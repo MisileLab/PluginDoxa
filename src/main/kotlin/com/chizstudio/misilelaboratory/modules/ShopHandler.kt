179674d40855953998a173de7dcbd912ed2938fc
MisileLab <misileminecord@gmail.com>
2021-12-16 00:28:57 +0900

    feat: setup basic gui, not working

--- src/main/kotlin/com/chizstudio/misilelaboratory/modules/ShopHandler.kt
+++ src/main/kotlin/com/chizstudio/misilelaboratory/modules/ShopHandler.kt
@@ -4,26 +4,27 @@ import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
-import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
-/*
-ShopHandler Class
-*/
+class ShopHandler(player: Player) {
+    private var inventoryhandler = InventoryHandler(player, 1, "상점")
+    fun setupguimain() {
+        inventoryhandler.changeinventory(Material.CRAFTING_TABLE, 1, name="&f상호 블럭", lore=convertstring("&f상호작용이 가능한 블럭들의 조합법을 익힐 수 있습니다."))
+        inventoryhandler.changeinventory(Material.DIAMOND_PICKAXE, 3, name="&f장비 및 도구", lore=convertstring("&f장비와 도구들의 조합법을 익힐 수 있습니다."))
+        inventoryhandler.changeinventory(Material.REDSTONE, 5, name="&f장비 및 도구", lore=convertstring("&f장비와 도구들의 조합법을 익힐 수 있습니다."))
+        inventoryhandler.changeinventory(Material.PAPER, 7, name="&f판매 및 구매", lore=convertstring("&f물건들을 판매하거나 구매할 수 있습니다."))
+    }
+
+    private fun convertstring(string: String): List<Component> {
+        return listOf(Component.text(string))
+    }
+}
 
 class InventoryHandler(holder: Player, y: Int, title: String) {
     private var selfinventory = Bukkit.createInventory(holder, (y * 8), Component.text(title))
     private var selfholder = holder
     private var selftitle = title
     private var selfy = y
-
-    fun returninventory(): Inventory {
-        return selfinventory
-    }
-
-    fun changeinventory(inventory: Inventory) {
-        selfinventory = inventory
-    }
 
     fun changey(y: Int, autoreload: Boolean = true) {
         selfy = y
@@ -32,6 +33,7 @@ class InventoryHandler(holder: Player, y: Int, title: String) {
         }
     }
 
+    @Suppress("unused")
     fun changeholder(holder: Player, autoreload: Boolean = true) {
         selfholder = holder
         if (autoreload) {
@@ -39,7 +41,7 @@ class InventoryHandler(holder: Player, y: Int, title: String) {
         }
     }
 
-    fun changetitle(title: String, autoreload: Boolean) {
+    fun changetitle(title: String, autoreload: Boolean = true) {
         selftitle = title
         if (autoreload) {
             reload()
@@ -50,22 +52,13 @@ class InventoryHandler(holder: Player, y: Int, title: String) {
         selfinventory = Bukkit.createInventory(selfholder, (selfy * 8), Component.text(selftitle))
     }
 
-    fun changeinventorylist(change: List<Any>, y: Int) {
-        val changelist = mutableListOf<Material>()
-        for (i in change) {
-            val i2: Material = if (i !is Material) {
-                Material.AIR
-            } else {
-                i
-            }
-            changelist.add(i2)
+    fun changeinventory(change: Material, index: Int, amount: Int = 1, name: String? = null, lore: List<Component>? = null) {
+        val itemstack = ItemStack(change, amount)
+        val itemmeta = itemstack.itemMeta
+        if (name != null) {
+            itemmeta.displayName(Component.text(name))
         }
-        changelist.forEachIndexed { index, material ->
-            changeinventory(material, index)
-        }
-    }
-
-    fun changeinventory(change: Material, index: Int, amount: Int=1) {
-        selfinventory.setItem(index, ItemStack(change, amount))
+        itemmeta.lore(lore)
+        selfinventory.setItem(index, itemstack)
     }
 }
